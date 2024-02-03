30b1eeb441e676c01bd246d539026d2b5abd8e1e
liamsi5 <96539094+liamsi5@users.noreply.github.com>
2024-02-03 21:07:57 +0100

    Update LoaderLogo.js

--- src/components/Loader/LoaderLogo.js
+++ src/components/Loader/LoaderLogo.js
@@ -7,9 +7,9 @@ class LogoLoader extends React.Component {
     return (
       <svg
         className="raw_logo"
-        width="50%"
-        height="40%"
-        viewBox="0 0 440 305"
+        width="408"
+        height="479"
+        viewBox="0 0 408 479"
         fill="none"
         xmlns="http://www.w3.org/2000/svg"
       >
